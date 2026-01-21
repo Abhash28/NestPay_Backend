6677d060030a650c9c3d40598dcc70a6cb7ee10a
Abhash Singh <abhashsingh28@gmail.com>
2026-01-21 17:00:45 +0530

    update cron

--- cron/rentDue.cron.js
+++ cron/rentDue.cron.js
@@ -4,14 +4,14 @@ const updateOverdueRent = require("../services/rentOverDue");
 
 // Generate rent (daily)
 cron.schedule(
-  "* * * * *", // 12:01 AM IST
+  "1 0 * * *", // 12:01 AM IST
   generateRentDue,
   { timezone: "Asia/Kolkata" },
 );
 
 // Mark overdue (daily)
 cron.schedule(
-  "* * * * *", // 12:05 AM IST
+  "5 0 * * *", // 12:05 AM IST
   updateOverdueRent,
   { timezone: "Asia/Kolkata" },
 );
